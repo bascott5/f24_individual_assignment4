4744dd9ab2271b435078a5151240a6a9e7e9d9ac
Bennett <bascott1002@gmail.com>
2024-10-22 12:57:51 -0400

    Created pages for API

--- src/main/java/com/csc340/demo/Animal/AnimalController.java
+++ src/main/java/com/csc340/demo/Animal/AnimalController.java
@@ -1,36 +1,59 @@
 package com.csc340.demo.Animal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/animals")
 public class AnimalController {
     @Autowired
     private AnimalService service;
 
     @GetMapping("/all")
-    public List<Animal> getAllAnimals() {
-        return service.getAllAnimals();
+    public String getAllAnimals(Model model) {
+        model.addAttribute("animalList", service.getAllAnimals());
+        model.addAttribute("title", "All Animals");
+        return "animal-list";
     }
 
     @GetMapping("/{animalId}")
-    public Animal getAnimal(@PathVariable int animalId) {
-        return service.getAnimal(animalId);
+    public String getAnimal(@PathVariable int animalId, Model model) {
+        model.addAttribute("animal", service.getAnimal(animalId));
+        model.addAttribute("title", animalId);
+        return "animal-details";
+    }
+
+    @GetMapping("/createForm")
+    public String showCreateForm() {
+        return "animal-create";
     }
 
     @PostMapping("/new")
-    public List<Animal> addAnimal(@RequestBody Animal animal) {
+    public String addAnimal(Animal animal) {
         service.addAnimal(animal);
-        return service.getAllAnimals();
+        return "redirect:/animals/all";
     }
 
-    @PutMapping("/update/{animalId}")
-    public Animal updateAnimal(@PathVariable int animalId, @RequestBody Animal animal) {
-        service.updateAnimal(animalId, animal);
-        return service.getAnimal(animalId);
+    @GetMapping("/update/{animalId}")
+    public String showUpdateForm(@PathVariable int animalId, Model model) {
+        model.addAttribute("animal", service.getAnimal(animalId));
+        return "animal-update";
+    }
+
+    @PostMapping("/update")
+    public String updateAnimal(Animal animal) {
+        service.addAnimal(animal);
+        return "redirect:/animals/" + animal.getAnimalId();
+    }
+
+    @GetMapping("/delete/{animalId}")
+    public String showDeleteForm(@PathVariable int animalId) {
+        service.deleteAnimal(animalId);
+        return "redirect:/animals/all";
     }
 
     @DeleteMapping("/delete/{animalId}")
@@ -40,13 +63,17 @@ public class AnimalController {
     }
 
     @GetMapping("/species/{species}")
-    public List<Animal> getAnimalsBySpecies(@PathVariable String species) {
-        return service.getAnimalsBySpecies(species);
+    public String getAnimalsBySpecies(@PathVariable String species, Model model) {
+        model.addAttribute("speciesList", service.getAnimalsBySpecies(species));
+        model.addAttribute("title", species);
+        return "animal-list";
     }
 
     @GetMapping("/search/{string}")
-    public List<Animal> searchAnimal(@PathVariable String string) {
-        return service.searchAnimal(string);
+    public String searchAnimal(@PathVariable String string, Model model) {
+        model.addAttribute("searchList", service.searchAnimal(string));
+        model.addAttribute("title", string);
+        return "animal-list";
     }
 
 }
